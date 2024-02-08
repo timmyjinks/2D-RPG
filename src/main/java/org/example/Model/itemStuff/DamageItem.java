ab8aa6f322296b1dfd602af721fe8fdcfa9f984b
timmy.jinks <114427348+timmyjinks@users.noreply.github.com>
2024-02-08 16:21:59 -0700

    add all Item behaviors

--- src/main/java/org/example/Model/itemStuff/DamageItem.java
+++ src/main/java/org/example/Model/itemStuff/DamageItem.java
@@ -1,6 +1,17 @@
 package org.example.Model.itemStuff;
 
-public class DamageItem {
-    int minDamage;
-    int maxDamage;
+public class DamageItem extends Item {
+    private final int minDamage;
+    private final int maxDamage;
+
+    public DamageItem(String name, int durability, int minDamage, int maxDamage) {
+        super(name, durability);
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+    }
+
+    @Override
+    public int use() {
+        return (int)Math.floor(Math.random() * (maxDamage - minDamage + 1) + minDamage);
+    }
 }
--- src/main/java/org/example/Model/itemStuff/DefenseItem.java
+++ src/main/java/org/example/Model/itemStuff/DefenseItem.java
@@ -1,4 +1,15 @@
 package org.example.Model.itemStuff;
 
 public class DefenseItem extends Item {
+    private final int defense;
+
+    public DefenseItem(String name, int durability, int defense) {
+        super(name, durability);
+        this.defense = defense;
+    }
+
+    @Override
+    public int use() {
+        return this.defense;
+    }
 }
