02d49ed454d63a5f292461bb239ef1518a4ab2b1
AnoyingWaffle66 <joboshua15@gmail.com>
2024-02-08 12:57:26 -0700

    added missing classes

---
+++ src/main/java/org/example/Model/Player.java
@@ -0,0 +1,8 @@
+package org.example.Model;
+
+import org.example.Model.itemStuff.Item;
+
+public class Player extends ABSCreature{
+    private Item healingItem, defenseItem, damageItem;
+    private int healingPotions = 0;
+}
