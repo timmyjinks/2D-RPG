ad787513d37a325c639c6a5a2c9409eb7c8ad163
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-02-12 12:55:28 -0700

    change item classes

--- src/main/java/org/example/Model/itemStuff/DamageItem.java
+++ src/main/java/org/example/Model/itemStuff/DamageItem.java
@@ -1,17 +1,18 @@
 package org.example.Model.itemStuff;
 
 public class DamageItem extends Item {
-    protected final int minDamage;
-    protected final int maxDamage;
+    protected int minDamage;
+    protected int maxDamage;
 
     public DamageItem(String name, int durability, int minDamage, int maxDamage) {
         super(name, durability);
-        this.minDamage = minDamage;
-        this.maxDamage = maxDamage;
+        setMinDamage(minDamage);
+        setMaxDamage(maxDamage);
     }
 
     @Override
     public int use() {
+        super.setDurability(1);
         return (int)Math.floor(Math.random() * (maxDamage - minDamage + 1) + minDamage);
     }
 
@@ -19,7 +20,27 @@ public class DamageItem extends Item {
         return this.minDamage;
     }
 
+    public void setMinDamage(int minDamage) {
+        if (minDamage <= 0) {
+            this.minDamage = 1;
+        } else if (minDamage > 30) {
+            this.minDamage = 30;
+        } else {
+            this.minDamage = minDamage;
+        }
+    }
+
     public int getMaxDamage() {
         return this.maxDamage;
+    }
+
+    public void setMaxDamage(int maxDamage) {
+        if (maxDamage <= 0) {
+            this.maxDamage = 1;
+        } else if (maxDamage > 30) {
+            this.maxDamage = 30;
+        } else {
+            this.maxDamage = maxDamage;
+        }
     }
 }
--- src/main/java/org/example/Model/itemStuff/HealingItem.java
+++ src/main/java/org/example/Model/itemStuff/HealingItem.java
@@ -1,15 +1,30 @@
 package org.example.Model.itemStuff;
 
 public class HealingItem extends  Item{
-    public final int healthPower;
+    public int healthPower;
 
     public HealingItem(String name, int durability, int healthPower) {
-        super(name, durability);
+        super(name, healthPower);
         this.healthPower = healthPower;
     }
 
     @Override
     public int use() {
-        return this.healthPower;
+        setDurability(1);
+        return getHealthPower();
+    }
+
+    public int getHealthPower() {
+        return healthPower;
+    }
+
+    public void setHealthPower(int healthPower) {
+        if (healthPower <= 0) {
+            this.healthPower = 1;
+        } else if (healthPower > 10) {
+            this.healthPower = 10;
+        } else {
+            this.healthPower = healthPower;
+        }
     }
 }
