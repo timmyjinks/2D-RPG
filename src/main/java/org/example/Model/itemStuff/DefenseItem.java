a1eae52c01e57edb365be9b0dc3faf5fbc48f3d2
timmy.jinks <114427348+timmyjinks@users.noreply.github.com>
2024-02-12 12:56:06 -0700

    change item classes

--- src/main/java/org/example/Model/itemStuff/DefenseItem.java
+++ src/main/java/org/example/Model/itemStuff/DefenseItem.java
@@ -1,7 +1,7 @@
 package org.example.Model.itemStuff;
 
 public class DefenseItem extends Item {
-    private final int defense;
+    private int defense;
 
     public DefenseItem(String name, int durability, int defense) {
         super(name, durability);
@@ -10,6 +10,21 @@ public class DefenseItem extends Item {
 
     @Override
     public int use() {
+        setDurability(1);
         return this.defense;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        if (defense <= 0) {
+            this.defense = 1;
+        } else if (defense > 15) {
+            this.defense = 15;
+        } else {
+            this.defense = defense;
+        }
     }
 }
--- src/main/java/org/example/Model/itemStuff/Item.java
+++ src/main/java/org/example/Model/itemStuff/Item.java
@@ -10,4 +10,20 @@ public abstract class Item {
     }
 
     public abstract int use();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public void setDurability(int itemDamage) {
+        this.durability -= itemDamage;
+    }
 }
