97c45b5a4fbdb09a03771936395eb299975fad4c
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-02-13 13:42:38 -0700

    add different weapon types

---
+++ src/main/java/org/example/Model/itemStuff/Spear.java
@@ -0,0 +1,7 @@
+package org.example.Model.itemStuff;
+
+public class Spear extends DamageItem {
+    public Spear(String name, int durability, int minDamage, int maxDamage) {
+        super(name, durability, 8, 20);
+    }
+}
---
+++ src/main/java/org/example/Model/itemStuff/Sword.java
@@ -0,0 +1,7 @@
+package org.example.Model.itemStuff;
+
+public class Sword extends DamageItem {
+    public Sword(String name, int durability, int minDamage, int maxDamage) {
+        super(name, durability, 5, 15);
+    }
+}
