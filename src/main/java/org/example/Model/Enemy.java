30abb5b9e55011d2ea1f3bda6dc83b076469ed78
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-02-14 11:29:12 -0700

    change creature classes, update README, change combatcontroller

--- src/main/java/org/example/Model/Enemy.java
+++ src/main/java/org/example/Model/Enemy.java
@@ -1,6 +1,10 @@
 package org.example.Model;
 
 public abstract class Enemy extends Creature {
+    public Enemy(String name, int health, int speed) {
+        super(name, health, speed);
+    }
+
     public boolean willDropItem() {
         return (int)(Math.random() * (50 + 1) + 1) == 1;
     }
