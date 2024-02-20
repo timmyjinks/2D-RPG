877f3aa0f4cab1613b4e75682f653a422c8bda09
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-02-20 08:51:01 -0700

    update to use libgdx

--- src/main/java/org/example/model/player/types/Wizard.java
+++
@@ -1,9 +0,0 @@
-package org.example.model.player.types;
-
-import org.example.model.Player;
-
-public class Wizard extends Player {
-    public Wizard(String name) {
-        super(name,0,0,0);
-    }
-}
