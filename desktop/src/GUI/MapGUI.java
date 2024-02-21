3ae820265a5861e13e086b8683598077412859ef
Khayden Roberts <122504123+Hoochymoochy@users.noreply.github.com>
2024-02-21 10:55:57 -0700

    Khay (#36)
    GUI PARTS part 1

---
+++ desktop/src/GUI/MapGUI.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import edu.sandwichproductions.Game;
+
+public class MapGUI {
+    public MapGUI() {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setWindowedMode(2200,1300);
+        config.setResizable(false);
+        config.setTitle("CSC150_FinalGame");
+        new Lwjgl3Application(new Game(), config);
+    }
+}
--- desktop/src/edu/sandwichproductions/DesktopLauncher.java
+++ desktop/src/edu/sandwichproductions/DesktopLauncher.java
@@ -1,21 +1,19 @@
 package edu.sandwichproductions;
 
-import com.badlogic.gdx.Graphics;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import GUI.StartingMenuGUI;
 
-import java.awt.*;
 
-import static com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration.getDisplayMode;
+import javax.swing.*;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		Graphics.DisplayMode displayMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
-		config.setForegroundFPS(60);
-		config.setWindowedMode(2500, 1400);
-		config.setTitle("CSC150_FinalGame");
-		new Lwjgl3Application(new Game(), config);
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new StartingMenuGUI();
+			}
+		});
+
 	}
 }
