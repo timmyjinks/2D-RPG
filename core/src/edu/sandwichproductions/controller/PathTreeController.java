183a746f6d6ee6a9e560607589dae08e1129f47e
Khayden Roberts <122504123+Hoochymoochy@users.noreply.github.com>
2024-02-23 19:28:08 -0700

    GUI Done (#43)
    Need to make the menu more pretty but it works how it should

--- core/src/edu/sandwichproductions/controller/PathTreeController.java
+++ core/src/edu/sandwichproductions/controller/PathTreeController.java
@@ -2,41 +2,73 @@ package edu.sandwichproductions.controller;
 
 import edu.sandwichproductions.view.UserChoice;
 
+import java.nio.file.attribute.PosixFileAttributes;
+
 
 public class PathTreeController {
     UserChoice get = new UserChoice();
-    private int num;
+    private int num, position;
     private String active;
-    private int position;
     private boolean finish = false;
-
-
     public void mainTree(){
-        if (position == 0){
-            branch(get.Forest(),get.Mountain(),get.Town(),1,2,3); // Picking first path
+        switch (position) {
+            case 0:
+                branch(get.Forest(), get.Mountain(), get.Town(), 1, 2, 3); // Picking first path
+                break;
+            case 1:
+                branch(get.Forest1(), get.Forest2(), get.Forest3(), 4, 5, 6); // Forest entry
+                break;
+            case 2:
+                branch(get.Mountain1(), get.Mountain2(), get.Mountain3(), 7, 8, 9); // Mountain entry
+                break;
+            case 3:
+                branch(get.Town1(), get.Town2(), get.Town3(), 10, 11, 12); // Town entry
+                break;
+            case 4:
+                getUserClass(get.Druid(), get.Rogue(), get.Paladin()); // Forest options 1
+                break;
+            case 5:
+                getUserClass(get.Druid(), get.Barbarian(), get.Druid()); // Forest options 2
+                break;
+            case 6:
+                getUserClass(get.Cleric(), get.Druid(), get.Cleric()); // Forest options 3
+                break;
+            case 7:
+                getUserClass(get.Monk(), get.Paladin(), get.Bard()); // Mountain options 1
+                break;
+            case 8:
+                getUserClass(get.Barbarian(), get.Wizard(), get.Bard()); // Mountain options 2
+                break;
+            case 9:
+                getUserClass(get.Cleric(), get.Wizard(), get.Monk()); // Mountain options 3
+                break;
+            case 10:
+                getUserClass(get.Barbarian(), get.Rogue(), get.Rogue()); // Town options 1
+                break;
+            case 11:
+                getUserClass(get.Monk(), get.Bard(), get.Rogue()); // Town options 2
+                break;
+            case 12:
+                getUserClass(get.Druid(), get.Wizard(), get.Paladin()); // Town options 3
+                break;
+            default:
+                break;
         }
-        else if(position == 1){
-            branch(get.Forest1(),get.Forest2(),get.Forest3(),4,5,6); // Forest entry
-        }
-        else if(position == 2){
-            branch(get.Mountain1(),get.Mountain2(),get.Mountain3(),7,8,9); // Mountain entry
-        }
-        else if(position == 3){
-            branch(get.Town1(),get.Town2(),get.Town3(),10,12,13); // Town entry
-        }
-        getUserClass();
     }
-    public void getUserClass(){
-        if(position == 4){
-            getUserClass(get.Druid());
-        }
-        else if(position == 5){
-            getUserClass(get.Rogue());
-        }
-        else if(position == 6){
-            getUserClass(get.Paladin());
-        }
 
+
+    public void getUserClass(String class1,String class2,String class3){
+        switch(num){
+            case 1:
+                getUserClass(class1);
+                break;
+            case 2:
+                getUserClass(class2);
+                break;
+            case 3:
+                getUserClass(class3);
+                break;
+        }
     }
     public void branch(String prompt1, String prompt2, String prompt3,int pos1, int pos2, int pos3){
         if(num == 0){
@@ -57,6 +89,8 @@ public class PathTreeController {
                 break;
         }
     }
+
+
     public void getUserClass(String userClass){
         activePrompt(userClass);
         finish = true;
--- desktop/src/GUI/StartGUI.java
+++ desktop/src/GUI/StartGUI.java
@@ -1,44 +1,30 @@
 package GUI;
 
 import edu.sandwichproductions.controller.PathTreeController;
-import edu.sandwichproductions.model.map.Map;
-import edu.sandwichproductions.view.UserChoice;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.File;
 
 public class StartGUI {
     PathTreeController con = new PathTreeController();
     public StartGUI(){
         JPanel promptArea = new JPanel();
-        JPanel path1Area = new JPanel();
-        JPanel path2Area = new JPanel();
-        JPanel path3Area = new JPanel();
 
         JTextArea prompt = new JTextArea();
 
         con.getNum(0);
         prompt.setText(con.getPrompt());
 
-
         promptArea.setLayout(new BorderLayout());
-
-
 
         prompt.setLineWrap(true); // Enable text wrapping
         prompt.setWrapStyleWord(true); // Wrap at word boundaries
         prompt.setEditable(false);
 
-
-
         JButton path1 = new JButton("Path1");
         JButton path2 = new JButton("Path2");
         JButton path3 = new JButton("Path3");
-
-
-
 
         path1.setBounds(300, 850, 250, 100);
         path1.setBackground(Color.GRAY);
@@ -52,93 +38,63 @@ public class StartGUI {
         path3.setBackground(Color.GRAY);
         path3.setFocusable(false);
 
-
-
-        path1Area.setBackground(Color.CYAN);
-        path2Area.setBackground(Color.magenta);
-        path3Area.setBackground(Color.red);
         promptArea.setBackground(Color.YELLOW);
 
-
-
-
-        path1Area.setBounds(225,500,400,200);
-        path2Area.setBounds(675,500,400,200);
-        path3Area.setBounds(1125,500,400,200);
         promptArea.setBounds(675,200,400,200);
 
-
-
-
-        Base base = new Base();
-
-
+        Base base = new Base(); // make the frame
 
         base.add(path1); // add the buttons to the forms
         base.add(path2);
         base.add(path3);
 
-
-
         promptArea.add(prompt);
-
         base.add(promptArea);
-        base.add(path1Area);
-        base.add(path2Area);
-        base.add(path3Area);
+        final boolean[] work = {false};
 
+        path1.addActionListener(e -> {
+            con.getNum(3);
+            prompt.setText(con.getPrompt());
+            con.getNum(0);
+            if(con.finish()) {
+                base.remove(path1);
+                base.remove(path3);
+                base.revalidate();
+                base.repaint();
+                path2.setText("Continue ahead");
 
-        path1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                con.getNum(1);
-                prompt.setText(con.getPrompt());
-                con.getNum(0);
-                if(con.finish()){
-                    base.remove(path1);
-                    base.remove(path3);
-                    base.revalidate();
-                    base.repaint();
-                    path2.setText("Continue ahead");
-                }
             }
         });
-        path2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        path2.addActionListener(e -> {
+            if(work[0]){
+                base.dispose();
+                new MapGUI();
+            }
+            else if(con.finish()){
+                base.remove(path1);
+                base.remove(path3);
+                base.revalidate();
+                base.repaint();
+                path2.setText("Continue ahead");
+                work[0] = true;
+            }
+            else{
                 con.getNum(2);
                 prompt.setText(con.getPrompt());
                 con.getNum(0);
-                if(con.finish()){
-                    base.remove(path1);
-                    base.remove(path3);
-                    base.revalidate();
-                    base.repaint();
-                    path2.setText("Continue ahead");
-                    path2.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            base.dispose();
-                            new MapGUI();
-                        }
-                    });
-                }
             }
         });
-        path3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        path3.addActionListener(e -> {
                 con.getNum(3);
                 prompt.setText(con.getPrompt());
                 con.getNum(0);
-                if(con.finish()){
+                if(con.finish()) {
                     base.remove(path1);
                     base.remove(path3);
                     base.revalidate();
                     base.repaint();
                     path2.setText("Continue ahead");
                 }
-            }
         });
     }
 }
