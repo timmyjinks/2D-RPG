808a83e52705ca28c0092180c859203d12d49cdd
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-02-26 16:36:43 -0700

    Add animation to enemies

--- core/src/edu/sandwichproductions/controller/AnimationHandler.java
+++ core/src/edu/sandwichproductions/controller/AnimationHandler.java
@@ -2,32 +2,30 @@ package edu.sandwichproductions.controller;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AnimationHandler {
-//    private Texture texture;
-//    private int rows, columns, frameCount;
-//    private String filePath;
-//    private float frameDuration;
-//    private Animation<TextureRegion> animated;
+    private Texture texture;
+    private int rows, columns, frameCount;
+    private String filePath;
+    private float frameDuration;
+    private Animation<TextureRegion> animated;
 
-    public static Animation<TextureRegion> animate(String filePath, int columns, int rows, int frameCount, float frameDuration){
-        Texture texture = new Texture(filePath);
-        TextureRegion[][] tmpFrames = TextureRegion.split(texture, texture.getWidth() / columns, texture.getHeight() / rows);
-        TextureRegion[] animationFrames = new TextureRegion[frameCount];
+    public AnimationHandler (String filePath, int columns, int frameCount) {
+        texture = new Texture(filePath);
+        TextureRegion[][] tmpFrames = TextureRegion.split(texture, texture.getWidth() / frameCount, texture.getHeight());
+        TextureRegion[] animationFrames = new TextureRegion[4];
         int frames = 0;
-        for (int rowsI = 0; rowsI < rows - 1; rows++){
-            for (int columnsI = 0; columnsI < columns - 1; columns++){
-                animationFrames[frames++] = tmpFrames[rows][columns];
+        for (int rowsI = 0; rowsI < 1; rowsI++) {
+            for (int columnsI = 0; columnsI < columns; columnsI++) {
+                animationFrames[frames++] = tmpFrames[rowsI][columnsI];
             }
         }
-         return new Animation<TextureRegion>(frameDuration, animationFrames);
+        animated = new Animation<>(0.1F, animationFrames);
     }
 
-//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//    elapsedTime += Gdx.graphics.getDeltaTime();
-//		batch.begin();
-//		batch.draw(skeletonIdle.getKeyFrame(elapsedTime, true), 1500f, 500f);
-//		batch.draw(skeletonIdle.getKeyFrame(elapsedTime,true), 1000f, 500f);
-
+    public TextureRegion getKeyFrame(float elapsedTime, boolean loops) {
+        return animated.getKeyFrame(elapsedTime, loops);
+    }
 }
