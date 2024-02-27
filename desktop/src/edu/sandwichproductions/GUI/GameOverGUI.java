577346cfd3d101ad8a174821d6fbb604c9890c76
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-02-27 13:39:51 -0700

    Update bugs with the GUI files

--- desktop/src/edu/sandwichproductions/GUI/GameOverGUI.java
+++ desktop/src/edu/sandwichproductions/GUI/GameOverGUI.java
@@ -5,11 +5,17 @@ import java.awt.event.*;
 
 public class GameOverGUI extends JDialog {
     public GameOverGUI() {
+        JFrame frame = new JFrame();
         JButton tryAgain = new JButton("Try Again");
         JButton quit = new JButton("Quit");
 
         tryAgain.setBounds(300, 150, 500, 500);
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+
+        frame.add(tryAgain);
 
         tryAgain.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
