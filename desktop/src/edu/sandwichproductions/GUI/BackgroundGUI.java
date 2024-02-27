3612144a97edc8bf93a42ec1cc64b3842a94715f
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-02-27 13:31:15 -0700

    Update to show game over screen when you die

---
+++ desktop/src/edu/sandwichproductions/GUI/BackgroundGUI.java
@@ -0,0 +1,16 @@
+package edu.sandwichproductions.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BackgroundGUI extends JPanel {
+    Image pic;
+    public BackgroundGUI(String fileName){
+        ImageIcon obj = new ImageIcon(fileName);
+        pic = obj.getImage();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(pic,0,0,1710,1030,null);
+    }
+}
--- desktop/src/edu/sandwichproductions/GUI/SettingGUI.java
+++ desktop/src/edu/sandwichproductions/GUI/SettingGUI.java
@@ -6,17 +6,11 @@ import java.awt.event.*;
 public class SettingGUI extends JDialog {
 
     public SettingGUI() {
-        Base base = new Base();
-
         JButton difficulty = new JButton("Difficulty");
         JButton setting = new JButton("Setting");
 
         difficulty.setBounds(0, 0, 500, 500);
         setting.setBounds(500,500, 500, 500);
-
-         base.add(difficulty);
-         base.add(setting);
-
 
         difficulty.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
