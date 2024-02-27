454d74eef53410aa3ee0079ae08a187734dac51e
timmy.jinks <114427348+timmyjinks@users.noreply.github.com>
2024-02-27 13:49:45 -0700

    Update bug fixes to GUI files
    * Update bugs with the GUI files
    
    * Update bugs with the GUI files

--- desktop/src/edu/sandwichproductions/GUI/GameOverGUI.java
+++ desktop/src/edu/sandwichproductions/GUI/GameOverGUI.java
@@ -5,26 +5,27 @@ import java.awt.event.*;
 
 public class GameOverGUI extends JDialog {
     public GameOverGUI() {
-        Base base = new Base();
-
+        JFrame frame = new JFrame();
         JButton tryAgain = new JButton("Try Again");
         JButton quit = new JButton("Quit");
 
         tryAgain.setBounds(300, 150, 500, 500);
 
-        base.add(tryAgain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
 
+        frame.add(tryAgain);
 
         tryAgain.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                base.dispose();
                 new StartingMenuGUI();
             }
         });
 
         quit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                base.dispose();
+                dispose();
             }
         });
     }
--- desktop/src/edu/sandwichproductions/GUI/SettingGUI.java
+++ desktop/src/edu/sandwichproductions/GUI/SettingGUI.java
@@ -6,11 +6,19 @@ import java.awt.event.*;
 public class SettingGUI extends JDialog {
 
     public SettingGUI() {
+        JFrame frame = new JFrame();
         JButton difficulty = new JButton("Difficulty");
         JButton setting = new JButton("Setting");
 
         difficulty.setBounds(0, 0, 500, 500);
         setting.setBounds(500,500, 500, 500);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+
+        frame.add(difficulty);
+        frame.add(setting);
 
         difficulty.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -20,7 +28,7 @@ public class SettingGUI extends JDialog {
 
         setting.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                base.dispose();
+                dispose();
                 new StartingMenuGUI();
             }
         });
