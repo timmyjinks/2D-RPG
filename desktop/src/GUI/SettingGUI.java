f98d2e492d396a0761b5ce3978daa5674abf4e55
Josh Arbon <jarbon@student.neumont.edu>
2024-02-29 07:55:30 -0700

    temporary commit because my laptop is fucked

---
+++ desktop/src/GUI/SettingGUI.java
@@ -0,0 +1,35 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SettingGUI extends JDialog {
+
+    public SettingGUI() {
+        JFrame base = new JFrame();
+
+        JButton difficulty = new JButton("Difficulty");
+        JButton setting = new JButton("Setting");
+
+        difficulty.setBounds(0, 0, 500, 500);
+        setting.setBounds(500,500, 500, 500);
+
+         base.add(difficulty);
+         base.add(setting);
+
+
+        difficulty.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("difficulty");
+            }
+        });
+
+        setting.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                base.dispose();
+                new StartingMenuGUI();
+            }
+        });
+    }
+}
