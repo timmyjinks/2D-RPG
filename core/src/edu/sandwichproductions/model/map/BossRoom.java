f1a26f27ac348ee0d35a364029293d19c45b35c8
timmy.jinks <114427348+timmyjinks@users.noreply.github.com>
2024-02-29 15:52:59 -0700

    Fix drawing inventory to cause high memory usage
    * Add displaying current inventory
    
    * Add displaying current inventory
    
    * Fix high memory usage

--- core/src/edu/sandwichproductions/model/map/BossRoom.java
+++ core/src/edu/sandwichproductions/model/map/BossRoom.java
@@ -10,7 +10,7 @@ public class BossRoom extends Room {
         this.isBossRoom = true;
         this.enemies = new Enemy[1];
         this.enemies[0] = new Skeleton("Ding Ding the Great");
-        this.enemies[0].setHealth(9999);
+        this.enemies[0].setHealth(999999999);
         this.enemies[0].setPositionInRoom(24);
         this.enemies[0].setSpeed(500);
     }
