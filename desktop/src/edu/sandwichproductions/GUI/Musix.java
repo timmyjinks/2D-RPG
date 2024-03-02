4abb0a8f8c7c2e65fe64ffa390036b23d3db0832
Khayden Roberts <Khaydenroberts22@gmail.com>
2024-03-01 17:10:32 -0700

    Added musiz

---
+++ desktop/src/edu/sandwichproductions/GUI/Musix.java
@@ -0,0 +1,26 @@
+package edu.sandwichproductions.GUI;
+import com.badlogic.gdx.Audio;
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Musix {
+    public void playAudio() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File("assets/omf.wav");
+        if (!file.exists()) {
+            System.err.println("Audio file not found.");
+            return;
+        }
+
+        while (true) {
+            AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(stream);
+            clip.start();
+            clip.drain();
+            clip.close();
+        }
+    }
+}
